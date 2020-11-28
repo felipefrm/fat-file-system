e2a0c346068c8e6da024817741c6de91b88c5554
Felipe <narulee@hotmail.com>
2020-11-28 16:15:24 -0300

    command, arg1, arg2

--- src/shell.c
+++ src/shell.c
@@ -12,7 +12,26 @@ int main() {
     FILE* fat_part = openFAT();
     while(1) {
         printf("fatshell~$ ");
-        getCommand(buffer, command, arg1, arg2);
+        setbuf(stdin, NULL);
+        fgets(buffer, sizeof(buffer), stdin);
+        buffer[strcspn(buffer, "\n")] = 0;
+        setbuf(stdin, NULL);
+
+        command[0] = '\0';
+        arg1[0] = '\0';
+        arg2[0] = '\0';
+
+        char *token = strtok(buffer, " ");
+        for (int i=0; token != NULL; i++) {
+            if (i == 0)
+                strcpy(command, token);
+            else if (i == 1)
+                strcpy(arg1, token);
+            else if (i == 2)
+                strcpy(arg2, token);
+            token = strtok(NULL, " ");
+        }
+
         if (STR_EQUAL(command, "init")) {
             init(fat_part);
         }
@@ -23,10 +42,17 @@ int main() {
                 fprintf(stderr, "fat must be initialized.\n");
         }
         else if (STR_EQUAL(command, "ls")){
-            // ls("teste");
+            if (arg1[0] != '\0') {
+                // ls(arg1);
+            }
+            else
+                fprintf(stderr, "ls [path/directory]\n");
         }
         else if (STR_EQUAL(command, "mkdir")){
-            mkdir(fat_part, "teste");
+            if (arg1[0] != '\0')
+                mkdir(fat_part, arg1);
+            else
+                fprintf(stderr, "mkdir [path/directory]\n");
         }
         else if (STR_EQUAL(command, "create")){}
         else if (STR_EQUAL(command, "unlink")){}
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,6 @@
 #ifndef UTILS
 #define UTILS
 
-void getCommand(char* buffer,char*  command,char*  arg1,char*  arg2);
+
 
 #endif
